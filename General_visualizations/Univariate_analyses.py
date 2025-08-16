ce0462bd91ed5e7a8ed85f2057f71277b6376cff
Tim Leiler <tim.leiler@outlook.de>
2025-08-16 19:21:10 +0200

    Update

---
+++ General_visualizations/Univariate_analyses.py
@@ -0,0 +1,15 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import plotly.graph_objects as go
+import plotly.express as px
+from plotly.subplots import make_subplots
+import plotly.figure_factory as ff
+from scipy import stats
+import openpyxl
+
+#Reading the file / Create the dataframe
+df = pd.read_excel("Auswertung.xlsx")
+
+print(df.head())
